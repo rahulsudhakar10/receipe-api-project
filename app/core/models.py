390d673ad04d76929a7982c56ebb8e7d2d41e281
rahulsudhakar10 <rahulsudhakar007@gmail.com>
2020-05-05 15:44:14 +0530

    code optismism by flake

--- app/core/models.py
+++ app/core/models.py
@@ -1,33 +1,34 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, \
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
 
 
 class UserManger(BaseUserManager):
 
-    def create_user(self,email,password,**extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Email was not present')
-        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,email,password):
-        user = self.create_user(email,password)
-        user.is_staff= True
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser,PermissionsMixin):
 
-    email = models.EmailField(max_length=234,unique=True)
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=234, unique=True)
     name = models.CharField(max_length=234)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManger()
 
-    USERNAME_FIELD= 'email'
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -3,39 +3,33 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTest(TestCase):
-
-
     def test_user_with_email(self):
-
-        email="rahul@gamil.com"
-        password="12345"
+        email = "rahul@gamil.com"
+        password = "12345"
 
         user = get_user_model().objects.create_user(
            email=email,
            password=password
-        )
-        self.assertEqual(user.email,email)
+           )
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_user_with_email_normilized(self):
+        email = "rahul@GMAIL.COM"
+        user = get_user_model().objects.create_user(
+          email, '12345')
 
-       email="rahul@GMAIL.COM"
-       user = get_user_model().objects.create_user(
-          email,'12345' )
-
-       self.assertEqual(user.email,email.lower())
+        self.assertEqual(user.email, email.lower())
 
     def test_user_with_no_email(self):
-
         with self.assertRaises(ValueError):
-             get_user_model().objects.create_user(
-           None,'12345' )
+            get_user_model().objects.create_user(
+              None, '12345')
 
     def test_user_is_superuser(self):
+        email = "rahul@GMAIL.COM"
+        user = get_user_model().objects.create_superuser(
+          email, '12345')
 
-       email="rahul@GMAIL.COM"
-       user = get_user_model().objects.create_superuser(
-          email,'12345' )
-
-       self.assertTrue(user.is_superuser)
-       self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
